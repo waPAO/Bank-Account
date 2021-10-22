987fb9b9acfb7d5ea73f1ad9d22691a8c3775781
Pao <pzsigua@gmail.com>
2021-10-22 13:47:38 -0700

    Update bankAccount.py

--- bankAccount.py
+++ bankAccount.py
@@ -1,13 +1,16 @@
 class BankAccount:
+    # Initialize object
     def __init__(self, full_name: str, account_number: int, balance = 0) -> None:
         self.name = full_name
         self.number = account_number
         self.balance = balance
-        
+    
+    # Deposit money that will be added into balance
     def deposit(self, amount: float) -> None:
         self.balance += amount
         print(f"Amount deposited: ${amount} new balance: ${self.balance}")
     
+    # Withdraw money that will be deducted from balance
     def withdraw(self, amount: float) -> None:
         if amount > self.balance:
             print("Insufficient funds")
@@ -16,14 +19,17 @@ class BankAccount:
             self.balance -= amount
             print(f"Amount withdrawn: ${amount} new balance: ${self.balance}")
 
+    # Returns balance and prints out balance
     def get_balance(self) -> float:
         print(f"Current Account Balance: ${self.balance}")
         return self.balance
     
+    # Add interest to balance
     def add_interest(self) -> None:
         interest = self.balance * 0.00083
         self.balance += interest
     
+    # Print account details
     def print_statement(self) -> None:
         print(f"{self.name}\nAccount No.: ****{str(self.number)[-4:]}\nBalance: ${self.balance}")
 
